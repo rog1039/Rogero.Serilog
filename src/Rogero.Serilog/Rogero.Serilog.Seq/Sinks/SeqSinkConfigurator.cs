5ea29f46787862ef2a241a791dc4a8b1bdfab39b
Paul Rogero <paulrogero@gmail.com>
2016-04-26 15:59:31 -0400

    Added buffer base file name config for Seq sink.

--- src/Rogero.Serilog/Rogero.Serilog.Seq/Sinks/SeqSinkConfigurator.cs
+++ src/Rogero.Serilog/Rogero.Serilog.Seq/Sinks/SeqSinkConfigurator.cs
@@ -9,18 +9,20 @@ namespace Rogero.Serilog.Seq.Sinks
         public LogEventLevel LogEventLevel { get; set; } = LogEventLevel.Verbose;
         public string ApiKey { get; set; } = null;
         public bool UseLogFileBuffers { get; set; } = true;
+        public string BufferBaseFileName { get; set; } = "logsBufferBase";
 
-        public SeqSinkConfigurator(string seqUrl, LogEventLevel logEventLevel = LogEventLevel.Verbose, string apiKey = null, bool useLogFileBuffers = true)
+        public SeqSinkConfigurator(string seqUrl, LogEventLevel logEventLevel = LogEventLevel.Verbose, string apiKey = null, bool useLogFileBuffers = true, string bufferBaseFileName = null)
         {
             SeqUrl = seqUrl;
             LogEventLevel = logEventLevel;
             ApiKey = apiKey;
             UseLogFileBuffers = useLogFileBuffers;
+            BufferBaseFileName = bufferBaseFileName ?? BufferBaseFileName;
         }
 
         public LoggerConfiguration Apply(LoggerConfiguration configuration)
         {
-            if(UseLogFileBuffers) return configuration.WriteTo.Seq(SeqUrl, LogEventLevel, apiKey: ApiKey, bufferBaseFilename:"logsBufferBase");
+            if(UseLogFileBuffers) return configuration.WriteTo.Seq(SeqUrl, LogEventLevel, apiKey: ApiKey, bufferBaseFilename:BufferBaseFileName);
             return configuration.WriteTo.Seq(SeqUrl, LogEventLevel, apiKey:ApiKey);
         }
     }
